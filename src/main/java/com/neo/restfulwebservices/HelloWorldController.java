06dab60b2e3b3ad1635e2e4a9fccfa264c45431c
Oleg <oleg@Olegs-MacBook-Pro.local>
2018-08-10 15:20:01 +0100

    create helloWorld controller

--- src/main/java/com/neo/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/neo/restfulwebservices/HelloWorldController.java
@@ -1,4 +1,14 @@
 package com.neo.restfulwebservices;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
 }
---
+++ src/test/java/com/neo/restfulwebservices/HelloWorldControllerTest.java
@@ -0,0 +1,22 @@
+//package com.neo.restfulwebservices;
+////
+////import org.junit.Assert;
+////import org.junit.Test;
+////import org.springframework.beans.factory.annotation.Autowired;
+////import org.springframework.test.web.servlet.MockMvc;
+////import org.springframework.test.web.servlet.MvcResult;
+////import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+////
+////import static org.junit.Assert.*;
+////
+////public class HelloWorldControllerTest {
+////
+////    @Autowired
+////    private MockMvc mockMvc;
+////
+////    @Test
+////    public void shouldReturnHelloWorld() {
+////        final MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/hello-world"));
+////        Assert.assertEquals(result.getResponse(), "Hello World");
+////    }
+////}
