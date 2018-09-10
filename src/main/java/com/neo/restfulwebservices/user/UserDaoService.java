70018be05cf2cae7bfae974f91cb549949848409
olegfkl <olegfkl@gmail.com>
2018-09-10 23:10:34 +0100

    Snapshot of Object Mapper implementation

--- src/main/java/com/neo/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/neo/restfulwebservices/user/UserDaoService.java
@@ -1,13 +1,28 @@
 package com.neo.restfulwebservices.user;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.nashorn.internal.parser.JSONParser;
+import org.json.simple.JSONObject;
+import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+
+import java.io.FileNotFoundException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 @Component
 public class UserDaoService {
+
+    private static final String filePath = "./user.json";
 
     private static Integer userCount = 3;
 
@@ -21,22 +36,47 @@ public class UserDaoService {
 
     public List<User> findAll() {
         return users;
-    };
+    }
+
+    ;
 
     public User save(User user) {
-        if(user.getId()==null){
+        if (user.getId() == null) {
             user.setId(++userCount);
         }
         users.add(user);
         return user;
-    };
+    }
+
+    ;
 
     public User findOne(int id) {
-        for(User user:users) {
-            if(user.getId()==id){
+        for (User user : users) {
+            if (user.getId() == id) {
                 return user;
             }
         }
         return null;
+    }
+
+
+    public void test() throws IOException {
+
+//        ObjectMapper mapper = new ObjectMapper();
+////        FileReader reader = new FileReader(filePath);
+//
+//      anything = mapper.readValue(new File("/Users/oleg/workspace/spring-microservice/src/main/java/com/neo/restfulwebservices/user/user.json"));
+//            return anything;
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        JSONParser parser = new JSONParser();
+//        // read the json file
+//
+//        FileReader reader = new FileReader(filePath);
+//
+//
+////
+//        JSONObject jsonObject =  parser.parse(reader);
+//
+//        User user = objectMapper.readValue(jsonObject, User.class);
     }
 }
--- src/main/java/com/neo/restfulwebservices/user/UserResource.java
+++ src/main/java/com/neo/restfulwebservices/user/UserResource.java
@@ -1,16 +1,23 @@
 package com.neo.restfulwebservices.user;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 
 public class UserResource {
+
+ private static List<User> users = new ArrayList<>();
 
     @Autowired
     private UserDaoService service;
@@ -18,6 +25,15 @@ public class UserResource {
     @GetMapping(path = "/users")
     public List<User> retrieveAllUsers() {
         return service.findAll();
+    }
+
+    @GetMapping(path = "/test")
+    public List<User> testIt() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+//        List<User> myObjects = mapper.readValue(jsonInput, new TypeReference<List<MyClass>>(){});
+
+        users = mapper.readValue(new File("/Users/oleg/workspace/spring-microservice/src/main/java/com/neo/restfulwebservices/user/user.json"), new TypeReference<List<User>>(){});
+        return users;
     }
 
     @GetMapping(path = "/users/{userId}")
